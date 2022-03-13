228935c82c383fb91c7520a74461d976f8511cf4
nowkoai <50360258+nowkoai@users.noreply.github.com>
2022-03-14 01:03:49 +0900

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,3 +1,13 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region = "ap-northeast-1"
+}
+
+
 resource "aws_s3_bucket" "bucket-public-read-write-acl" {
   bucket = "bucket-public-read-write-acl-0314"
   acl    = "public-read-write"
